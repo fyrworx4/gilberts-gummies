33621013c071792c1ddd0dcc80d2a8c7365b9bb2
Taylor Nguyen <fyrworx@gmail.com>
2022-04-15 11:52:50 -0700

    draft 1

--- otp.py
+++ otp.py
@@ -1,4 +1,5 @@
 import math
+from unittest import result
 
 string = "Hello"
 key = "world"
@@ -8,12 +9,19 @@ def toBinary(a):
   for i in a:
     l.append(ord(i))
   for i in l:
-    m.append(str(bin(i)[2:]))
+    tes = (str(bin(i)[2:]))
+    if (len(tes) < 8):
+        tes = "0" + tes
+    #print(tes)
+    m.append(tes)
   return m
 
-#print(toBinary("Hello world"))
-binary_string = ' '.join(toBinary(string))
-binary_key = ' '.join(toBinary(key))
+# print(toBinary("Hello world"))
+binary_string = ''.join(toBinary(string))
+binary_key = ''.join(toBinary(key))
 
 print(binary_string)
 print(binary_key)
+
+result = int(binary_string) ^ int(binary_key)
+print(result)
